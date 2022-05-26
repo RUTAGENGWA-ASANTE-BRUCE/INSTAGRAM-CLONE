0f51a001ce6d8d49fa0ab071085d092dc2cfc2af
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-05-26 09:38:07 +0200

    new changes

---
+++ instagram-clone/src/home/createPost/CreatePost.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./CreatePost.css"
+
+function CreatePost ()
+{
+    return (
+        <div className='create_post'>
+            <h2>Create A Post</h2>
+            
+        </div>
+    )
+}
+
+export default CreatePost
